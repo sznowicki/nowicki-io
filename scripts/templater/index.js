a8cc7ff991ecb6ec3fc56b8ab094733dcd95f4d9
Szymon Nowicki <sznowicki@eskalacja.com>
2022-11-02 19:48:27 +0100

    feat: ensure unique css

--- scripts/templater/index.js
+++ scripts/templater/index.js
@@ -1,13 +1,20 @@
 const pug = require('pug');
 const { resolve } = require('path');
+const crypto = require('crypto');
 const render = require('./reactRender.jsx');
 
 const compiledTemplate = pug.compileFile(resolve(__dirname, './templates/layout.pug'));
 
+const hashLong = crypto.createHmac("md5", `unique - ${Date.now()}`).digest('hex');
+const hash = hashLong.slice(0, 5);
+
 const makeDocument = async (type, props, meta) => {
   const content = render(type, props);
   const output = compiledTemplate({
-    meta,
+    meta: {
+      ...meta,
+      unique: hash,
+    },
     content,
   });
 
